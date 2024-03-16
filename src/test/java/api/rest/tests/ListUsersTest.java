7552c41d0438ed90ea69b1f73ba13856ed515b89
mullernicolli <niscolem@hotmail.com>
2024-03-16 02:03:13 -0300

    Test API Restassured listUsers

---
+++ src/test/java/api/rest/tests/ListUsersTest.java
@@ -0,0 +1,28 @@
+package api.rest.tests;
+
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class ListUsersTest {
+    @Test
+    public void testGetUsersPerPage() {
+        // Configurar o caminho de acesso a API Rest
+        baseURI = "https://reqres.in/api";
+
+        // Listar usuários por página
+        given()
+                .param("page", 1)
+                .param("per_page", 6)
+            .when()
+                .get("/users")
+            .then()
+                .statusCode(200)
+                .body("total", equalTo(12))
+                .body("page", equalTo(1))
+                .body("per_page", equalTo(6))
+                .body("total_pages", equalTo(2))
+                .body("data.size()", equalTo(6));
+    }
+}
